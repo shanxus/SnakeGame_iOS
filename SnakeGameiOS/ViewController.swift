e94248a0902b6808575d88419e68e7c035fd988c
Shan_Chen <tarasuzy00@gmail.com>
2021-05-29 14:18:26 +0800

    First commit

---
+++ SnakeGameiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnakeGameiOS
+//
+//  Created by Shan OvO on 2021/5/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

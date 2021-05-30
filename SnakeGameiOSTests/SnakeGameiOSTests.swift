62a06c2f1c9e450bbaa7ca251c52be0f0231d59b
Shan_Chen <tarasuzy00@gmail.com>
2021-05-30 22:37:07 +0800

    Add some test cases.
    [Why] To add unit test for Direction and NodePositionUtils.

--- SnakeGameiOSTests/SnakeGameiOSTests.swift
+++ SnakeGameiOSTests/SnakeGameiOSTests.swift
@@ -17,17 +17,74 @@ class SnakeGameiOSTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    // MARK: - Test for Direction.
+    func testGetOppositeDirectionHorizontally() {
+        XCTAssertTrue(Direction.left.getOpposite() == Direction.right)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetOppositeDirectionVertically() {
+        XCTAssertTrue(Direction.up.getOpposite() == Direction.down)
     }
-
+    
+    // MARK: - Test for NodePositionUtils
+    func testIsAtSamePosition() {
+        let leftNode: NodePosition = (0, 0)
+        let rightNode: NodePosition = (0, 0)
+        XCTAssertTrue(NodePositionUtils.isAtSamePosition(leftNode: leftNode, rightNode: rightNode))
+    }
+    
+    func testNotAtSamePosition() {
+        let leftNode: NodePosition = (0, 0)
+        let rightNode: NodePosition = (1, 0)
+        XCTAssertFalse(NodePositionUtils.isAtSamePosition(leftNode: leftNode, rightNode: rightNode))
+    }
+    
+    func testGetNodeRightPosition() {
+        let origin: NodePosition = (0, 0)
+        let right: NodePosition = (1, 0)
+        XCTAssertTrue(NodePositionUtils.getNodePosition(from: origin, toward: .right) == right)
+    }
+    
+    func testGetNodeLeftPosition() {
+        let origin: NodePosition = (0, 0)
+        let left: NodePosition = (-1, 0)
+        XCTAssertTrue(NodePositionUtils.getNodePosition(from: origin, toward: .left) == left)
+    }
+    
+    func testGetNodeUpPosition() {
+        let origin: NodePosition = (0, 0)
+        let up: NodePosition = (0, -1)
+        XCTAssertTrue(NodePositionUtils.getNodePosition(from: origin, toward: .up) == up)
+    }
+    
+    func testGetNodeDownPostion() {
+        let origin: NodePosition = (0, 0)
+        let down: NodePosition = (0, 1)
+        XCTAssertTrue(NodePositionUtils.getNodePosition(from: origin, toward: .down) == down)
+    }
+    
+    func testGetNodeDirectionForRight() {
+        let origin: NodePosition = (0, 0)
+        let right: NodePosition = (1, 0)
+        XCTAssertTrue(NodePositionUtils.getNodeDirection(targetNode: origin, previousNode: right) == Direction.right)
+    }
+    
+    func testGetNodeDirectionForLeft() {
+        let origin: NodePosition = (0, 0)
+        let left: NodePosition = (-1, 0)
+        XCTAssertTrue(NodePositionUtils.getNodeDirection(targetNode: origin, previousNode: left) == Direction.left)
+    }
+    
+    func testGetNodeDirectionForUp() {
+        let origin: NodePosition = (0, 0)
+        let up: NodePosition = (0, -1)
+        XCTAssertTrue(NodePositionUtils.getNodeDirection(targetNode: origin, previousNode: up) == Direction.up)
+    }
+    
+    func testGetNodeDirectionForDown() {
+        let origin: NodePosition = (0, 0)
+        let down: NodePosition = (0, 1)
+        XCTAssertTrue(NodePositionUtils.getNodeDirection(targetNode: origin, previousNode: down) == Direction.down)
+    }
 }
